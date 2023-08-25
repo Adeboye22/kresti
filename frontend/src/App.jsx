ce2b89e1155b3b2cff94985a8daf96c4119e22ee
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2023-08-25 17:28:24 +0100

    navbar created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
+import Services from './components/Services'
 import Login from './components/Login'
+import NavBar from './components/Navbar'
+import Signup from './components/Signup'
 
 function App() {
   return (
     <>
+    <NavBar/>
     <Routes>
       <Route path='/' element={<Home/>} />
+      <Route path='/services' element={<Services/>} />
       <Route path='/login-page' element={<Login/>} />
+      <Route path='/signup-page' elements={<Signup/>} />
     </Routes>   
     </>
   )
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
+
+    // this function changes the color of the active button
+    const activeBtn = ({isActive}) => {
+        return{
+            color: isActive? "blue": "#38383b"
+        }
+    }
     return(
-        <div>
-            <div>
-                <h1></h1>
+        <div className='flex flex-row justify-between p-4'>
+            <div className='pl-8'>
+                <h1 className='text-xl text-blue font-extrabold'><NavLink to="/">Kresti Insurance Agency LLC</NavLink></h1>
             </div>
-            <div>
-                <ul>
-                    <li>Homepage</li>
-                    <li>Services</li>
+            <div className='pr-8'>
+                <ul className='flex space-x-16 text-color-gray text-lg'>
+                    <li><NavLink to="/" style={activeBtn}>Homepage</NavLink></li>
+                    <li><NavLink to="services" style={activeBtn}>Services</NavLink></li>
                 </ul>
+            </div>
+            <div className='flex space-x-12 pr-8 text-lg'>
+                <button className='bg-blue text-white py-0.5 px-6 rounded'>Login</button>
+                <span className='text-gray'><NavLink to="signup" style={activeBtn}>Sign Up</NavLink></span>
             </div>
         </div>
     )

4bb306280ba552414c84110e41fe5350e012c526
Oluwadamilola Oyeniya <oyeniyaoluwadamilola@outlook.com>
2023-08-25 02:34:52 +0100

    carousels

---
+++ frontend/src/components/Carousel.jsx
@@ -0,0 +1,36 @@
+import { Carousel } from "@material-tailwind/react";
+import Img1 from '../assets/Img1.jpg'
+import Img2 from '../assets/Img2.jpg'
+import Img3 from '../assets/Img3.jpg'
+import Img4 from '../assets/Img4.jpg'
+import Img5 from '../assets/Img5.jpg'
+
+ 
+function CarouselCustomNavigation() {
+  return (
+    <Carousel transition={{ duration: 2 }}>
+      <img
+        src={Img1}
+        alt="image 1"
+        className="h-full w-full object-cover bg-blend-lighten"
+      />
+      <img
+        src={Img2}
+        alt="image 2"
+        className="h-full w-full object-cover"
+      />
+      <img
+        src={Img3}
+        alt="image 3"
+        className="h-full w-full object-cover"
+      />
+     <img
+        src={Img4}
+        alt="image 4"
+        className="h-full w-full object-cover"
+      />
+    </Carousel>
+  );
+}
+
+export default CarouselCustomNavigation;
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
+import Carousel from './Carousel'
 
 const Home = () => {
     return(
-        <div className='bg-black min-h-screen'>
-            <h1 className='text-white'>Hi there</h1>
+        <div className='bg-blue min-h-screen'>
+            <main >
+                <div className='h-100 overflow-hidden relative'>
+                    <Carousel />
+                </div>
+                <div className='h-100 bg-blue absolute'></div>
+                <div>
+                    <span className='min-h-96'>get me</span>
+                    <div>jay</div>
+                    <div>nam</div>
+                    <div>lam</div>
+                    <div>askl</div>
+                    <div> msd</div>
+                    <div>ljlkda</div>
+                    <div>asdiojl</div>
+                    <div>asd</div>
+                    <div>asfafds</div>
+                    <div>asfaffa</div>
+                    <div>asfdegr</div>
+                    <div>fdset</div>
+                    <div>efsr</div>
+                    <div>asd</div>
+                    <div>sdf</div>
+                    <div>asdf</div>
+                </div>
+            </main>
         </div>
     )
 }
